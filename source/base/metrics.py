fix gradient metric and loss, add learned loss weighting. some bugs remaining...

source/base/metrics.py
@@ -11,38 +11,64 @@ def rmse(predictions: np.ndarray, targets: np.ndarray):
     return np.sqrt(((predictions - targets) ** 2).mean())
 
 
-def psnr(prediction: 'torch.Tensor', target: 'torch.Tensor', max_val: float = 255) -> 'torch.Tensor':
+def psnr(prediction: 'torch.Tensor', target: 'torch.Tensor', max_val: float = 1.0) -> 'torch.Tensor':
+    # max_val 1.0 for floats [0, 1], 255.0 for uint8
     import torch
     mse = torch.mean((prediction - target) ** 2)
-    return 10 * torch.log10(max_val ** 2 / mse)
+    psnr_value = 10 * torch.log10(max_val ** 2 / mse)
+    return psnr_value
 
 def gradient_rmse(prediction: 'torch.Tensor', target: 'torch.Tensor'):
+    """Compute RMSE of Sobel gradients between prediction and target, ignoring NaNs in target.
+    :param prediction: tensor of shape (N, C, H, W)
+    :param target: tensor of shape (N, C, H, W) with possible NaNs indicating unknown values to ignore
+    :return: tensor of shape (N,) with RMSE values for each sample in the batch
+    """
+    import torch
+    
+    error = gradient_loss_masked(prediction, target)
+    error = error ** 2
+    error = torch.sqrt(error.mean())
+        
+    return error
+
+def gradient_loss_masked(prediction: 'torch.Tensor', target: 'torch.Tensor'):
+    """Compute squared error of Sobel gradients between prediction and target, ignoring NaNs in target.
+    :param prediction: tensor of shape (N, C, H, W)
+    :param target: tensor of shape (N, C, H, W) with possible NaNs indicating unknown values to ignore
+    :return: tensor of shape (N, C, H, W) with squared error values for each sample in the batch
+    """
     import kornia
     import torch
     import torch.nn.functional as F
-    # kornia.filters.sobel computes the spatial gradient magnitude
     
-    pred_edges = kornia.filters.sobel(prediction)  # [B, C, H, W]
+    pred_edges = kornia.filters.sobel(prediction) 
     gt_edges = kornia.filters.sobel(target)
     
-    error = (pred_edges - gt_edges)**2
+    error = (pred_edges - gt_edges)
     
-    valid_mask = ~torch.isnan(target)
-    if valid_mask.sum() > 0:
+    nan_mask = torch.isnan(target)
+    
+    if nan_mask.any():
         # erode mask to get the Sobel filter convolutions to be valid
         kernel = torch.ones((1, 1, 3, 3), device=target.device, dtype=torch.float32)
-        mask_float = valid_mask[:, 0:1, :, :].float()
+        mask_float = nan_mask[:, 0:1, :, :].float()
         with torch.no_grad():
             neighborhood_sum = F.conv2d(mask_float, kernel, padding=1)
-            eroded_mask = (neighborhood_sum == 9.0)
+            eroded_mask = (neighborhood_sum > 0.0)
             
             # Expand back to match channel dimension (e.g., 3 for RGB)
             eroded_mask = eroded_mask.expand_as(target)
-        error_valid = error[eroded_mask]
-    else:
-        error_valid = error.flatten()  # all nans, just return something to avoid error
+        error[eroded_mask] = 0.0
         
-    return torch.sqrt(error_valid.mean())
+        # # Handle case where eroded mask is still empty (NaNs at borders)
+        # if error_valid.numel() == 0:
+        #     return torch.tensor(0.0, device=target.device, dtype=target.dtype)
+        if torch.isnan(error).any():
+            raise ValueError('NaNs still present in error after masking')
+        
+    return error
+    
 
 def lpips(prediction: 'torch.Tensor', target: 'torch.Tensor',
           net_type: typing.Literal["vgg", "alex", "squeeze"] = "alex") \
@@ -79,3 +105,9 @@ def lpips(prediction: 'torch.Tensor', target: 'torch.Tensor',
     from torchmetrics.functional.image.lpips import _lpips_update
     loss, total = _lpips_update(prediction, target, lpips.__dict__[model_member_str].net, normalize=True)
     return loss
+
+def learned_loss_weighting(loss: 'torch.Tensor', weight: 'torch.Tensor') -> 'torch.Tensor':
+    """Learnable multi-task loss weighting from https://openaccess.thecvf.com/content_cvpr_2018/papers/Kendall_Multi-Task_Learning_Using_CVPR_2018_paper.pdf"""
+    import torch
+    weighted_loss = torch.exp(-weight) * loss + weight
+    return weighted_loss

source/modules/ipes_base.py
@@ -18,10 +18,13 @@ class IpesBase(BaseModule):
 
         # self.lr = 0.001  # for lr tuner, not sure if this is used afterward
         self.test_step_outputs = []
-        self.keys_to_log = frozenset({'abs_dist_rmse_ms'})
+        self.keys_to_log = frozenset({'hm_rmse_ms', 'hm_gradient_rmse'})
         self.regressor = self.make_regressor()
 
         self.predict_batch_size = predict_batch_size
+        
+        self.hm_loss_weight = nn.Parameter(torch.zeros(1))
+        self.hm_grad_loss_weight = nn.Parameter(torch.zeros(1))
 
     @abc.abstractmethod
     def make_regressor(self):
@@ -75,6 +78,14 @@ class IpesBase(BaseModule):
         return hm_seam_loss
 
     @staticmethod
+    def compute_loss_hm_gradient(pred, batch_data):
+        from source.base.metrics import gradient_loss_masked
+        hm_target = batch_data['hm_gt_ps'].clone()
+        hm_gradient_loss = gradient_loss_masked(pred.unsqueeze(1), hm_target.unsqueeze(1))[:, 0]  # temp channel dim
+        hm_gradient_loss = torch.clip(hm_gradient_loss, min=0.0, max=1.0)
+        return hm_gradient_loss
+    
+    @staticmethod
     def compute_loss_mean(pred, batch_data):
         mean_loss = nn.functional.mse_loss(input=pred, target=batch_data['hm_mean'], reduction='none')
         mean_loss = torch.clip(mean_loss, min=0.0, max=0.01)
@@ -83,6 +94,7 @@ class IpesBase(BaseModule):
 
     def compute_loss(self, pred, batch_data):
         import math
+        from source.base.metrics import learned_loss_weighting
 
         # keep same order as in yaml
         loss_components = [
@@ -90,10 +102,16 @@ class IpesBase(BaseModule):
             # IpesBase.compute_loss_gradient(pred, batch_data),
             # self.compute_loss_hm_seam(hm_loss),
             # IpesBase.compute_loss_mean(pred, batch_data),
+        
+            IpesBase.compute_loss_hm_gradient(pred, batch_data),
         ]
 
         loss_components_mean = [torch.mean(loss) for loss in loss_components]
-
+        
+        # learned weighting for heights
+        loss_components_mean[0] = learned_loss_weighting(loss_components_mean[0], self.hm_loss_weight)
+        loss_components_mean[1] = learned_loss_weighting(loss_components_mean[1], self.hm_grad_loss_weight)
+            
         loss_components = torch.stack(loss_components)
         loss_components_mean = torch.stack(loss_components_mean)
         loss_tensor = loss_components_mean.mean()

source/modules/ipes_rgbd.py
@@ -23,6 +23,9 @@ class IpesRgbd(IpesBase):
         
         self.keys_to_log = self.keys_to_log.union(frozenset({'rgb_psnr', 'rgb_gradient_rmse'}))
         
+        self.rgb_loss_weight = nn.Parameter(torch.zeros(1))
+        self.rgb_grad_loss_weight = nn.Parameter(torch.zeros(1))
+        
 
     def compute_loss_rgb_sparse(self, pred, batch_data):
         # 'works' with only query points, no GT RGB maps necessary
@@ -133,31 +136,47 @@ class IpesRgbd(IpesBase):
         w = rgb_target.shape[3]
         rgb_loss = rgb_loss[:, None, None].broadcast_to((b, h, w))
         return rgb_loss
-
+    
+    @staticmethod
+    def compute_loss_rgb_gradient(pred, batch_data):
+        from source.base.metrics import gradient_loss_masked
+        rgb_target = batch_data['rgb_gt'].clone()
+        rgb_gradient_loss = gradient_loss_masked(pred, rgb_target)
+        rgb_gradient_loss = torch.clip(rgb_gradient_loss, min=0.0, max=1.0)
+        rgb_gradient_loss = rgb_gradient_loss.sum(1)  # sum over RGB channels
+        return rgb_gradient_loss
+    
     def compute_loss(self, pred, batch_data):
         import math
+        from source.base.metrics import learned_loss_weighting
 
         # keep same order as in yaml
-        loss_components = [IpesBase.compute_loss_hm(pred[:, 0], batch_data),]
+        loss_tensor, loss_components_mean, loss_components = super().compute_loss(pred[:, 0], batch_data)
 
         if self.has_color_output:
-            loss_components += [
+            new_loss_components = [
                 # self.compute_loss_rgb_sparse(pred[:, 1:4], batch_data),
-                IpesRgbd.compute_loss_rgb(pred[:, 1:4], batch_data),
-                # IpesRgbd.compute_loss_rgb_huber(pred[:, 1:4], batch_data),
+                # IpesRgbd.compute_loss_rgb(pred[:, 1:4], batch_data),
+                IpesRgbd.compute_loss_rgb_huber(pred[:, 1:4], batch_data),
                 # IpesRgbd.compute_loss_rgb_l1(pred[:, 1:4], batch_data),
                 # IpesRgbd.compute_loss_rgb_lpips(pred[:, 1:4], batch_data),
                 # IpesRgbd.compute_loss_rgb_ssim(pred[:, 1:4], batch_data),
+                
+                IpesRgbd.compute_loss_rgb_gradient(pred[:, 1:4], batch_data),
             ]
 
-        # loss_components += [IpesBase.compute_loss_hm_seam(loss_components[0]),]
-
-        loss_components_mean = [torch.mean(loss) for loss in loss_components]
-
-        loss_components = torch.stack(loss_components)
-        loss_components_mean = torch.stack(loss_components_mean)
-        loss_tensor = loss_components_mean.mean()
-
+            new_loss_components_mean = [torch.mean(loss) for loss in new_loss_components]
+            
+            # learned weighting for RGB
+            new_loss_components_mean[0] = learned_loss_weighting(new_loss_components_mean[0], self.rgb_loss_weight)
+            new_loss_components_mean[1] = learned_loss_weighting(new_loss_components_mean[1], self.rgb_grad_loss_weight)
+            
+            new_loss_components_mean = [torch.mean(loss) for loss in new_loss_components_mean]
+            loss_components_mean = torch.cat((loss_components_mean, torch.stack(new_loss_components_mean)))
+            loss_tensor = loss_components_mean.mean()
+            
+            loss_components = torch.cat((loss_components, torch.stack(new_loss_components_mean)))
+        
         if math.isclose(loss_tensor.item(), 0.0):
             print('loss is close to zero')
 
@@ -190,7 +209,7 @@ class IpesRgbd(IpesBase):
             rgb_rmse = torch.sqrt(torch.mean(torch.square(rgb_e)))
 
             from source.base.metrics import psnr, lpips, gradient_rmse
-            rgb_psnr = psnr(pred_rgb_flat_no_nan, rgb_target_flat_no_nan, 255.0)
+            rgb_psnr = psnr(pred_rgb_flat_no_nan, rgb_target_flat_no_nan, 1.0)
             rgb_gradient_rmse = gradient_rmse(pred_rgb, rgb_target)
 
             # ignore all nans (fill from prediction)